20f6b7425b665088f3c45b04d06e419acd51cce3
Tiffany Rossi <tiffanyrossi@gmail.com>
2024-04-10 21:58:21 -0300

    ajustes perfil e template

--- pages/sobre/index.js
+++ pages/sobre/index.js
@@ -8,6 +8,13 @@ import photo from 'public/images/foto.jpeg';
 const siteTitle = "Sobre";
 
 export default function Sobre() {
+
+    var dob = new Date("07/03/1991");
+    var monthDiff = Date.now() - dob.getTime();
+    var ageDt = new Date(monthDiff);
+    var year =  ageDt.getUTCFullYear();
+    var age = Math.abs(year - 1970);
+
     return (
         <Layout siteTitle={siteTitle}>
             <h2>[tiffany rossi]</h2>
@@ -21,21 +28,21 @@ export default function Sobre() {
                     alt="Tiffany Rossi"
                 />
             <p>
-                oi! eu sou a Tiffany, <strong>desenvolvedora full-stack</strong>. em 2022, me formei em <i>desenvolvimento back-end</i> pela UNOPAR, mas logo no primeiro estágio descobri que gosto mesmo é de desenvolver aplicações do início ao fim - ou do front ao back end :)
+                oi! eu sou a Tiffany, <strong>desenvolvedora java</strong>. em 2022, me formei em <i>desenvolvimento back-end</i> pela UNOPAR, e logo no primeiro estágio descobri que também gosto muito de brincar com front-end :)
             </p>
             <p>
-                atualmente trabalho com desenvolvimento java em um grande banco, desenvolvendo APIs REST. meu objetivo é me tornar uma desenvolvedora melhor - para isso, estou reforçando aprendizados antigos e agregando novos conhecimentos. acredito no poder de <Link href="/posts/aprender-em-publico">aprender em público</Link>, por isso uso esse blog como documentação pessoal dos meus estudos.
+                atualmente trabalho em um grande banco, desenvolvendo APIs REST. meu objetivo é me tornar uma desenvolvedora melhor - para isso, estou reforçando aprendizados antigos e agregando novos conhecimentos. acredito no poder de <Link href="/posts/aprender-em-publico">aprender em público</Link>, por isso uso esse blog como documentação pessoal dos meus estudos.
             </p>
             <p>
-                para além do profissional, sou apaixonada por artes visuais, cozinhar e viajar. sou gateira e cachorreira, tomo mais café do que deveria e minha comida preferida é tofu. tenho 32 anos e antes de vir para a tecnologia era dona de uma micro-produtora de audiovisual. falo inglês fluente, e estou aprendendo espanhol. um dia espero conseguir falar italiano e, quem sabe, também aprender um pouco de alemão.
+                para além do profissional, sou apaixonada por artes visuais, cozinhar e viajar. sou gateira e cachorreira, tomo mais café do que deveria e minha comida preferida é tofu. tenho {age} anos e antes de vir para a tecnologia era dona de uma micro-produtora de audiovisual. falo inglês fluente, e estou aprendendo espanhol. um dia espero conseguir falar italiano e, quem sabe, também aprender um pouco de alemão.
             </p>
             <p>
-                me dá um oi no <a href="https://twitter.com/tiffsrc" target="_blank"><FaTwitter className={styles.icon} />twitter</a> se quiser jogar conversa fora, falar sobre transição de carreira e desenvolvimento ou trocar receitas veganas. se o assunto for trabalho, me chama no <a href="https://linkedin.com/in/tiffanyrossi" target="_blank"><FaLinkedin className={styles.icon} />linkedin</a>. já se quiser ver minhas saladonas e foto de gato e cachorro, meu <a href="https://instagram.com/tiffsrc" target="_blank"><FaInstagram className={styles.icon} />instagram</a> é o canal.
+                me dá um oi no <a href="https://twitter.com/tiffsrc" target="_blank"><FaTwitter className={styles.icon} />twitter</a> se quiser jogar conversa fora, falar sobre transição de carreira e desenvolvimento ou trocar receitas veganas. se o assunto for trabalho, me chama no <a href="https://linkedin.com/in/tiffanyrossi" target="_blank"><FaLinkedin className={styles.icon} />linkedin</a>.
             </p>
 
             <h3>conhecimentos</h3>
             <p>
-                java • javascript • html • css • typescript • sql • spring boot • apache camel • react • nextjs • apache camel • git • github • jenkins • agile/scrum • jira • confluence
+                java • javascript • html • css • typescript • sql • spring boot • apache camel • react • nextjs • apache camel • camunda bpmn • git • github • jenkins • agile/scrum • jira • confluence
             </p>
 
             <h3>blog</h3>
